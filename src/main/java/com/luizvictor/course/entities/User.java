1f958b8810964dc761d439e8e4bce12eecebfbc6
Luiz Victor <luizvictor.dev@outlook.com>
2023-10-16 21:55:39 -0300

    Mapping entity with jpa

--- src/main/java/com/luizvictor/course/entities/User.java
+++ src/main/java/com/luizvictor/course/entities/User.java
@@ -1,13 +1,18 @@
-package com.luizvictor.course.entities.user;
+package com.luizvictor.course.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/luizvictor/course/resources/UserResource.java
+++ src/main/java/com/luizvictor/course/resources/UserResource.java
@@ -1,17 +1,9 @@
 package com.luizvictor.course.resources;
 
-import com.luizvictor.course.entities.user.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "John Doe", "email@email.com", "111", "1234");
-        return ResponseEntity.ok().body(user);
-    }
 }
