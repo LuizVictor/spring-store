d0da6d34a5d0b7fd234fb4dc328e674261274365
Luiz Victor <luizvictor.dev@outlook.com>
2023-10-17 18:43:46 -0300

    creating user service

--- src/main/java/com/luizvictor/course/resources/UserResource.java
+++ src/main/java/com/luizvictor/course/resources/UserResource.java
@@ -1,9 +1,31 @@
 package com.luizvictor.course.resources;
 
+import com.luizvictor.course.entities.User;
+import com.luizvictor.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
