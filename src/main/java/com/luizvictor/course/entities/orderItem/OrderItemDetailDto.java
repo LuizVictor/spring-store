4dd47775a875f398b0f92611439bd86048a08d04
Luiz Victor <luizvictor.dev@outlook.com>
2023-11-01 20:13:36 -0300

    improving the details show of items

--- src/main/java/com/luizvictor/course/entities/orderItem/OrderItemDetailDto.java
+++ src/main/java/com/luizvictor/course/entities/orderItem/OrderItemDetailDto.java
@@ -3,11 +3,11 @@ package com.luizvictor.course.entities.orderItem;
 
 import java.math.BigDecimal;
 
-public record OrderItemDetailDto(Long id, Long productId, Integer quantity, BigDecimal price, BigDecimal subtotal) {
+public record OrderItemDetailDto(String name, String category, Integer quantity, BigDecimal price, BigDecimal subtotal) {
     public OrderItemDetailDto(OrderItem orderItem) {
         this(
-                orderItem.getId(),
-                orderItem.getProduct().getId(),
+                orderItem.getProduct().getName(),
+                orderItem.getProduct().getCategory().getName(),
                 orderItem.getQuantity(),
                 orderItem.getPrice(),
                 orderItem.getSubTotal()
