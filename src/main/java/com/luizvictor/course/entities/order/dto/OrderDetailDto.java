793dc18a539e32dc6c6776f6125be4286d08a5e8
Luiz Victor <luizvictor.dev@outlook.com>
2023-11-01 20:12:15 -0300

    Showing order total price

--- src/main/java/com/luizvictor/course/entities/order/dto/OrderDetailDto.java
+++ src/main/java/com/luizvictor/course/entities/order/dto/OrderDetailDto.java
@@ -3,14 +3,16 @@ package com.luizvictor.course.entities.order.dto;
 import com.luizvictor.course.entities.order.Order;
 import com.luizvictor.course.entities.orderItem.OrderItemDetailDto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public record OrderDetailDto(Long id, Long userId, List<OrderItemDetailDto> items, String status) {
+public record OrderDetailDto(Long id, Long userId, List<OrderItemDetailDto> items, BigDecimal total, String status) {
     public OrderDetailDto(Order order) {
         this(
                 order.getId(),
                 order.getUser().getId(),
                 order.getItens().stream().map(OrderItemDetailDto::new).toList(),
+                order.getTotal(),
                 order.getOrderStatus().name()
         );
     }
