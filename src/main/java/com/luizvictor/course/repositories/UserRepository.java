c4c1ae3123ea9241bd222764312971c834b0e82b
Luiz Victor <luizvictor.dev@outlook.com>
2023-11-02 15:04:33 -0300

    Adding spring security and making stateless

--- src/main/java/com/luizvictor/course/repositories/UserRepository.java
+++ src/main/java/com/luizvictor/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.luizvictor.course.repositories;
 
 import com.luizvictor.course.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByName(String name);
 }
---
+++ src/main/java/com/luizvictor/course/services/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.luizvictor.course.services;
+
+import com.luizvictor.course.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByName(username);
+    }
+}
